4e5fb637be097878f59eeb5d69e6d23f8d097081
Luiz Felipe Barros <luizfelipe_silv@hotmail.com>
2022-04-27 19:13:44 -0300

    feat - first board component implementation

---
+++ src/components/organisms/Board.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { IDeck } from './Deck';
+
+export interface IBoard {
+    deck1: IDeck;
+    deck2: IDeck;
+}
+
+export function Board (props: IBoard) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
