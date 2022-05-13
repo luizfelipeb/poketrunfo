391e30a8c2fba0e757b73e20ab18bf988ad93ea3
Henrry Esposito <henrrygesposito@gmail.com>
2022-05-12 22:00:26 -0300

    feat - landign page card rendering

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,22 +1,45 @@
-import * as React from 'react';
-// import { useBoardContext } from '../../context/board/useBoard';
+import React, { useEffect, useState } from 'react';
 import { usePokeApi } from '../../service/pokeApi/usePokeApi';
+import { IAttribute } from '../molecules/Attribute';
+import Card from '../molecules/Card';
 
 export function LandingPage() {
-  // const { startGame } = useBoardContext();
-
   const { consumePokeApi } = usePokeApi();
+  const [pokemon, setPokemon] = useState<any>();
+  const [stats, setStats] = useState<IAttribute[]>([]);
+
+  useEffect(() => {
+    consumePokeApi().then((res) => {
+      setPokemon(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (pokemon) {
+      setStats(pokemon.stats);
+    }
+  }, [pokemon]);
+
+  function parseStatsToAtributes(statsArray: any[]): IAttribute[] {
+    const atributes: IAttribute[] = [];
+
+    statsArray.forEach((value) => {
+      atributes.push({ name: value.stat.name, value: value.base_stat });
+    });
+
+    return atributes;
+  }
 
   return (
     <div>
       <h1>Poketrunfo</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Alias deleniti aperiam quisquam at necessitatibus fugit
-        maxime expedita iure cumque doloremque nobis nihil et,
-        minus laborum quo tenetur voluptatibus. Officiis, quisquam.
-      </p>
-      <button type="button" onClick={consumePokeApi}>Iniciar Jogo</button>
+      {pokemon && (
+      <Card
+        attributes={parseStatsToAtributes(stats)}
+        pokemonName={pokemon.name}
+        imageSrc={pokemon.sprites.front_default}
+      />
+      )}
     </div>
   );
 }
