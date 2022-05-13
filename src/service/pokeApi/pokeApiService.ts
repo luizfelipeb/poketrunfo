cf3317f81a5c4d5ac16b587b236bd30830edd21b
Henrry Esposito <henrrygesposito@gmail.com>
2022-05-12 22:49:37 -0300

    feat - get randon pokemon

--- src/service/pokeApi/pokeApiService.ts
+++ src/service/pokeApi/pokeApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function consumePokeApi() {
-  const resp = await axios.get(`${process.env.POKE_API_URL}/100`);
+  const resp = await axios.get(`${process.env.POKE_API_URL}/${Math.floor(Math.random() * (151 - 1 + 1) + 1)}`);
 
   return resp;
 }
