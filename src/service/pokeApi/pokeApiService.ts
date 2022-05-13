438aa33eb41aedf3672ff716cb53460a010ed921
Henrry Esposito <henrrygesposito@gmail.com>
2022-05-12 22:01:01 -0300

    feat - get one pokemon from pokeapi

--- src/service/pokeApi/pokeApiService.ts
+++ src/service/pokeApi/pokeApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function consumePokeApi() {
-  const resp = await axios.get(`${process.env.POKE_API_URL}`);
+  const resp = await axios.get(`${process.env.POKE_API_URL}/100`);
 
   return resp;
 }
